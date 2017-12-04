95ce0f160dc2b00707ea1d9de6d854c4df813abc
Chris Seto <chriskseto@gmail.com>
2017-12-04 11:15:34 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,21 @@
-# http://marianposaceanu.com/articles/macos-sierra-upgrade-from-a-developers-perspective
-# https://discussions.apple.com/thread/7682417?start=0&tstart=0
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 12
+#!/usr/bin/env bash
+set -xe  # Print out commands as they run and exit at first failure
+
+if [ "$(uname)" == "Darwin" ]; then
+  # http://marianposaceanu.com/articles/macos-sierra-upgrade-from-a-developers-perspective
+  # https://discussions.apple.com/thread/7682417?start=0&tstart=0
+  defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+  defaults write NSGlobalDomain KeyRepeat -int 1
+  defaults write NSGlobalDomain InitialKeyRepeat -int 12
+fi
 
 chsh -s /bin/zsh
 git submodule init
 git submodule update
+
+# Stops Stow from eating the .config directory
+mkdir -p ~/.config/
+
 stow zsh
 stow nvim
 stow git
