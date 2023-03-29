8286601c0a326306688f8701276b4de14feee96f
Chris Seto <chriskseto@gmail.com>
2023-03-29 22:31:51 +0000

    upgrade nas configurations

--- configurations/nas.nix
+++ configurations/nas.nix
@@ -4,29 +4,83 @@
   ...
 }: {
   config = {
-    fileSystems."/external" = {
-      device = "/dev/disk/by-uuid/e746c1ef-68ba-4855-beef-34d657cf629b";
-      fsType = "btrfs";
-    };
-
-    fileSystems."/var/lib" = {
-      device = "/dev/disk/by-uuid/e746c1ef-68ba-4855-beef-34d657cf629b";
-      fsType = "btrfs";
-      options = ["subvol=var_lib"];
-    };
-
     # Create a shared group for all NAS services
     users.groups.nasdaemons = {};
+
+    systemd.mounts = [
+      {
+        description = "External Drive Pool";
+        what = "/dev/disk/by-uuid/9f2b8872-d9c0-4762-a09a-97e82d2c8d48";
+        where = "/external";
+        options = "noatime,nodatacow";
+        type = "btrfs";
+        wantedBy = ["sabnzbd.service" "radarr.service" "sonarr.service"];
+        partOf = ["sabnzbd.service" "radarr.service" "sonarr.service"];
+      }
+      {
+        description = "Sabnzbd configuration";
+        what = "/dev/disk/by-uuid/9f2b8872-d9c0-4762-a09a-97e82d2c8d48";
+        where = "/var/lib/sabnzbd";
+        options = "subvol=configs/sabnzbd";
+        type = "btrfs";
+        wantedBy = ["sabnzbd.service"];
+        partOf = ["sabnzbd.service"];
+      }
+      {
+        description = "Radarr configuration";
+        what = "/dev/disk/by-uuid/9f2b8872-d9c0-4762-a09a-97e82d2c8d48";
+        where = "/var/lib/radarr";
+        options = "subvol=configs/radarr";
+        type = "btrfs";
+        partOf = ["radarr.service"];
+        wantedBy = ["radarr.service"];
+      }
+      {
+        description = "Sonarr configuration";
+        what = "/dev/disk/by-uuid/9f2b8872-d9c0-4762-a09a-97e82d2c8d48";
+        where = "/var/lib/sonarr";
+        options = "subvol=configs/sonarr";
+        type = "btrfs";
+        partOf = ["sonarr.service"];
+        wantedBy = ["sonarr.service"];
+      }
+      {
+        description = "Jellyfin configuration";
+        what = "/dev/disk/by-uuid/9f2b8872-d9c0-4762-a09a-97e82d2c8d48";
+        where = "/var/lib/jellyfin";
+        options = "subvol=configs/jellyfin";
+        type = "btrfs";
+        partOf = ["jellyfin.service"];
+        wantedBy = ["jellyfin.service"];
+      }
+    ];
+
+    # The override here was taken from jellyfin's config. It causes systemd to
+    # automatically chown these directories. Not sure how effective this actually is.
+
+    systemd.services.radarr.serviceConfig.StateDirectory = "radarr";
+    systemd.services.radarr.serviceConfig.StateDirectoryMode = "0700";
+
+    systemd.services.sabnzbd.serviceConfig.StateDirectory = "sabnzbd";
+    systemd.services.sabnzbd.serviceConfig.StateDirectoryMode = "0700";
+
+    systemd.services.sonarr.serviceConfig.StateDirectory = "sonarr";
+    systemd.services.sonarr.serviceConfig.StateDirectoryMode = "0700";
 
     # TODO: systemd has this build into the serviceConfig. See Jellyfin's
     # config.
     systemd.tmpfiles.rules = [
-      "d /external/data 0755 root nasdaemons - -"
-      "d /external/data/downloads/complete 0775 sabnzbd nasdaemons - -"
-      "d /external/data/downloads/incomplete 0775 sabnzbd nasdaemons - -"
-      "d /external/data/movies 0775 root nasdaemons - -"
-      "d /external/data/shows 0775 root nasdaemons - -"
-      "d /var/lib/prowlarr 0700 prowlarr nasdaemons - -"
+      "d /external/media/movies 0775 root nasdaemons - -"
+      "d /external/media/tv-shows 0775 root nasdaemons - -"
+      "d /external/downloads/complete 0775 root nasdaemons - -"
+      "d /external/downloads/incomplete 0775 root nasdaemons - -"
+      #   "d /external/data/downloads/complete 0775 sabnzbd nasdaemons - -"
+      #   "d /external/data/downloads/incomplete 0775 sabnzbd nasdaemons - -"
+      #   "d /external/data/movies 0775 root nasdaemons - -"
+      #   "d /external/data/shows 0775 root nasdaemons - -"
+      #   "d /var/lib/prowlarr 0700 prowlarr nasdaemons - -"
+      #   "d /var/lib/radarr 0700 radarr nasdaemons - -"
+      #   "d /var/lib/sonarr 0700 sonarr nasdaemons - -"
     ];
 
     networking.firewall = {
@@ -38,9 +92,9 @@
         # configure the subpaths for each service and disable ingress. sabnzbd
         # automatically serves itself under /sabnzbd, so no configuration is
         # needed.
+        # 6767 # bazarr
         # 8989 # sonarr
         # 7878 # radarr
-        # 9696 # prowlarr
         # 8080 # sabnzbd
       ];
       allowedUDPPorts = [
@@ -125,20 +179,6 @@
         isSystemUser = true;
         group = "nasdaemons";
         home = "/var/lib/prowlarr";
-      };
-    };
-
-    systemd.services.prowlarr = {
-      description = "Prowlarr";
-      after = ["network.target"];
-      wantedBy = ["multi-user.target"];
-
-      serviceConfig = {
-        Type = "simple";
-        User = "prowlarr";
-        Group = "nasdaemons";
-        ExecStart = "${pkgs.prowlarr}/bin/Prowlarr -nobrowser -data=/var/lib/prowlarr";
-        Restart = "on-failure";
       };
     };
   };
