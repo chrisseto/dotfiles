434e51f5495382af165db758f44a4a13181dc59f
Chris Seto <chriskseto@gmail.com>
2023-11-14 15:30:46 +0000

    Configure flash.nvim

--- assets/nvim/init.lua
+++ assets/nvim/init.lua
@@ -57,8 +57,25 @@ require("lazy").setup({
 		end,
 	},
 	-- Movement helpers
-	{ "ggandor/leap.nvim", opts = {} },
-	{ "ggandor/flit.nvim", opts = {} },
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+		-- stylua: ignore
+		keys = {
+			{ "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+			{ "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc =
+			"Flash Treesitter" },
+			{ "r",     mode = "o",               function() require("flash").remote() end,            desc =
+			"Remote Flash" },
+			{ "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end,
+				                                                                                          desc =
+				"Treesitter Search" },
+			{ "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc =
+			"Toggle Flash Search" },
+		},
+	},
 	{
 		-- Colorscheme. Configured to load before everything else.
 		'sainnhe/everforest',
