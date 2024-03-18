70f72c26912538018f30e16cf886f9401927572d
Chris Seto <chriskseto@gmail.com>
2024-03-18 09:57:46 -0400

    Add redpanda specific home

--- homes/redpanda.nix
+++ homes/redpanda.nix
@@ -4,4 +4,21 @@
   ...
 }: let
 in {
+  home.packages = [
+    pkgs.awscli
+    pkgs.chart-testing
+    pkgs.colima
+    pkgs.cue
+    pkgs.docker
+    pkgs.dyff
+    pkgs.go-task
+    pkgs.google-cloud-sdk
+    pkgs.k3d
+    pkgs.kind
+    pkgs.kubectl
+    pkgs.kubernetes-helm
+    pkgs.kustomize
+    pkgs.pandoc
+    pkgs.yq
+  ];
 }
