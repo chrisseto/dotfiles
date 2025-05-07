e95345ed781e4909bbe44a4c1b17b8a3d8d65743
Chris Seto <chriskseto@gmail.com>
2025-05-07 18:12:38 -0400

    rust LSP works better

--- assets/nvim/lua/plugins/lang/rust.lua
+++ assets/nvim/lua/plugins/lang/rust.lua
@@ -11,26 +11,6 @@ return {
 		'neovim/nvim-lspconfig',
 		opts = {
 			setup = {
-				rust_analyzer = function()
-					return true
-				end,
-			},
-		},
-	},
-	{
-		'mrcjkb/rustaceanvim',
-		version = '^5', -- Recommended
-		lazy = false, -- This plugin is already lazy
-		ft = { "rust" },
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"williamboman/mason-lspconfig.nvim",
-			"williamboman/mason.nvim",
-		},
-		opts = {
-			server = {
-				default_settings = {
-					-- rust-analyzer language server configuration
 					["rust-analyzer"] = {
 						cargo = {
 							allFeatures = true,
@@ -50,11 +30,48 @@ return {
 							},
 						},
 					},
-				},
 			},
 		},
-		config = function(_, opts)
-			vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
-		end
 	},
+	-- Conceptually a nice plugin but stable configuration has been nearly impossible as it conflicts with every other plugin.
+	-- {
+	-- 	'mrcjkb/rustaceanvim',
+	-- 	version = '^5', -- Recommended
+	-- 	lazy = false, -- This plugin is already lazy
+	-- 	ft = { "rust" },
+	-- 	dependencies = {
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 		"williamboman/mason-lspconfig.nvim",
+	-- 		"williamboman/mason.nvim",
+	-- 	},
+	-- 	opts = {
+	-- 		server = {
+	-- 			default_settings = {
+	-- 				-- rust-analyzer language server configuration
+	-- 				["rust-analyzer"] = {
+	-- 					cargo = {
+	-- 						allFeatures = true,
+	-- 						loadOutDirsFromCheck = true,
+	-- 						buildScripts = {
+	-- 							enable = true,
+	-- 						},
+	-- 					},
+	-- 					-- Add clippy lints for Rust.
+	-- 					checkOnSave = true,
+	-- 					procMacro = {
+	-- 						enable = true,
+	-- 						ignored = {
+	-- 							["async-trait"] = { "async_trait" },
+	-- 							["napi-derive"] = { "napi" },
+	-- 							["async-recursion"] = { "async_recursion" },
+	-- 						},
+	-- 					},
+	-- 				},
+	-- 			},
+	-- 		},
+	-- 	},
+	-- 	config = function(_, opts)
+	-- 		vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
+	-- 	end
+	-- },
 }
